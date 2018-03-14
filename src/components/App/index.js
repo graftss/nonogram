c7ad6dd4c9cc22efc6787f7276392667a882b33f
Nick Fireman <nfireman1@gmail.com>
2018-03-14 01:55:44 -0400

    center grid on page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 
-import './App.css';
 import Grid from '../Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
-          <Grid />
-        </div>
+        <Grid />
       </div>
     );
   }
