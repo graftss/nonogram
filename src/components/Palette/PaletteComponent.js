74fba074ccf4feea2fc16912997eb6671d66e354
Nick Fireman <nfireman1@gmail.com>
2018-03-15 01:54:25 -0400

    add palette hotkey indicators

--- src/components/Palette/PaletteComponent.js
+++ src/components/Palette/PaletteComponent.js
@@ -12,12 +12,15 @@ export default ({
   setActiveCellState,
 }) => (
   <div className="palette-container">
-    {palette.map(data => (
-      <div
-        key={data.cellState}
-        className={getClassName(activeCellState, data)}
-        onClick={() => setActiveCellState(data.cellState)}
-      >
+    {palette.map((data, index) => (
+      <div>
+        <span className="palette-hotkey">{index + 1}</span>
+        <div
+          key={data.cellState}
+          className={getClassName(activeCellState, data)}
+          onClick={() => setActiveCellState(data.cellState)}
+        >
+        </div>
       </div>
     ))}
   </div>
